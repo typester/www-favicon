1f52ef39a55fe1628c84349722854660bb4a2d40
Daisuke Murase <typester@cpan.org>
2008-05-02 14:35:47 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:

--- lib/WWW/Favicon.pm
+++ lib/WWW/Favicon.pm
@@ -8,7 +8,7 @@ use LWP::UserAgent;
 use HTML::TreeBuilder;
 use HTML::ResolveLink;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our @EXPORT_OK = qw/detect_favicon_url/;
 
 __PACKAGE__->mk_accessors(qw/ua/);
